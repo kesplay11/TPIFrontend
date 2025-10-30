48da5f6989eea688e898ff349cc69156be68548e
KevinMorales <rm8206304@gmail.com>
2025-10-30 19:21:03 -0300

    Empece con la etapa del frontend por ahora solo instale las dependencias mas importantes y pensando la mejor opcion para la estructura de carpetas para el proyecto, asi tambien, como encarar las diferentes vistas, diseño de componenentes, diseño de hooks personalizados, etc

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+    import * as React from 'react';
+import Box from '@mui/material/Box';
+import Fab from '@mui/material/Fab';
+import AddIcon from '@mui/icons-material/Add';
+import EditIcon from '@mui/icons-material/Edit';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import NavigationIcon from '@mui/icons-material/Navigation';
+import '@fontsource/roboto/700.css';
+import './App.css'
+
+function App() {
+  return(
+    <>
+
+        <h1 class="text-3xl font-bold underline">
+    Importe las dependencias
+    socet.io  
+    axios
+    tailwind.css 
+    mui/material  
+    roboto fonts que la usa muimaterial
+    el siguiente coommit sera para declarar la estructura del fronten(carpetas,servicios,etc)
+  </h1>
+
+</>
+  )
+}
+
+export default App
